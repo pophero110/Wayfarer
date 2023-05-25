70f67cbc8f742b7b3f46f7568d6a7758e8080f26
vnpugh <vnp19@yahoo.com>
2023-05-24 19:30:42 -0500

    Created Callout Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LogoComponent } from './logo/logo.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { TopicsComponent } from './topics/topics.component';
+import { CalloutComponent } from './callout/callout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogoComponent,
     GalleryComponent,
-    TopicsComponent
+    TopicsComponent,
+    CalloutComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/callout/callout.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CalloutComponent } from './callout.component';
+
+describe('CalloutComponent', () => {
+  let component: CalloutComponent;
+  let fixture: ComponentFixture<CalloutComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CalloutComponent]
+    });
+    fixture = TestBed.createComponent(CalloutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
