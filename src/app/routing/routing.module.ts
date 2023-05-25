90c82ec29b2165fc3bccd4666e7250e7d015a9b8
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-25 12:15:54 -0500

    Add /cities path to route

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from '../landing-page/landing-page.component';
+import { CityPageComponent } from '../city-page/city-page.component';
 
-const routes: Routes = [{ path: '', component: LandingPageComponent }];
+// https://angular.io/guide/router#displaying-a-404-page
+// Documentation for using router
+const routes: Routes = [
+  { path: 'cities', component: CityPageComponent },
+  { path: '**', component: LandingPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
