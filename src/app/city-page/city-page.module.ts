a062f650759120697996dbc6c872d1dd0a9936a6
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-26 10:03:52 -0500

    Fix compliation error

--- src/app/city-page/city-page.module.ts
+++ src/app/city-page/city-page.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { CityDetailComponent } from './city-detail/city-detail.component';
 import { CityListComponent } from './city-list/city-list.component';
 import { CityPageComponent } from './city-page-component/city-page.component';
+import { RoutingModule } from '../routing/routing.module';
 
 @NgModule({
   declarations: [CityDetailComponent, CityListComponent, CityPageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RoutingModule],
   exports: [CityDetailComponent, CityListComponent, CityPageComponent],
 })
 export class CityPageModule {}
