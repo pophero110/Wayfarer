6931c49ad1d74edd25de673de9e05cf75f435f15
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-26 18:37:00 -0500

    Refactor coode

--- src/app/city-page/city-page-component/city-page.component.ts
+++ src/app/city-page/city-page-component/city-page.component.ts
@@ -1,29 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { CityService } from '../../services/city.service';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-city-page',
   templateUrl: './city-page.component.html',
   styleUrls: ['./city-page.component.css'],
 })
-export class CityPageComponent implements OnInit {
-  cityId: any;
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private cityService: CityService
-  ) {}
-
-  ngOnInit() {
-    this.route.paramMap.subscribe((params) => {
-      const id = params.get('id');
-      if (id !== null) {
-        if (this.cityService.findCityById(id)) {
-          this.cityId = id;
-        } else {
-          this.router.navigate(['/404']);
-        }
-      }
-    });
-  }
-}
+export class CityPageComponent {}
