32369c239a0a3b3061972d09da206d8b0d645d73
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-25 21:16:23 -0500

    Refactor Navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,28 +1,7 @@
 import { Component } from '@angular/core';
-import { Post, posts } from '../data/posts';
-import { City, cities } from '../data/cities';
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
-  searchQuery: string = '';
-  filteredPosts: Post[] = [];
-  filteredCities: City[] = [];
-
-  search() {
-    console.log('Searching for:', this.searchQuery);
-
-    this.filteredPosts = posts.filter(post =>
-      post.title.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-      post.content.toLowerCase().includes(this.searchQuery.toLowerCase())
-    );
-
-    this.filteredCities = cities.filter(city =>
-      city.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-      city.id.toLowerCase().includes(this.searchQuery.toLowerCase())
-    );
-  }
-}
+export class NavbarComponent {}
