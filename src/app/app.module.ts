459c2ab5a68ccd63f2cb3b0588e402a828dcccef
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-25 21:26:32 -0500

    Refactor code using module to organize components from landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LogoComponent } from './logo/logo.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { TopicsComponent } from './topics/topics.component';
-import { CalloutComponent } from './callout/callout.component';
 import { CityPageComponent } from './city-page/city-page.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import { RoutingModule } from './routing/routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CityListComponent } from './city-list/city-list.component';
@@ -14,15 +10,12 @@ import { CityDetailComponent } from './city-detail/city-detail.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FormsModule } from '@angular/forms';
 import { SearchQueryDirective } from './directives/search-query.directive';
+import { LandingPageModule } from './landing-page/landing-page.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogoComponent,
-    GalleryComponent,
-    TopicsComponent,
-    CalloutComponent,
-    LandingPageComponent,
     NavbarComponent,
     CityPageComponent,
     CityListComponent,
@@ -30,7 +23,7 @@ import { SearchQueryDirective } from './directives/search-query.directive';
     PageNotFoundComponent,
     SearchQueryDirective,
   ],
-  imports: [BrowserModule, RoutingModule, FormsModule],
+  imports: [BrowserModule, RoutingModule, FormsModule, LandingPageModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent } from '../landing-page/landing-page.component';
+import { LandingPageComponent } from '../landing-page/landing-page-component/landing-page.component';
 import { CityPageComponent } from '../city-page/city-page.component';
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 
