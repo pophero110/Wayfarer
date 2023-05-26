2fa9d07a809b4e3902e11252b14d9280a5fe86c5
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-26 13:28:34 -0500

    Format code

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -5,14 +5,11 @@ import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css']
+  styleUrls: ['./weather.component.css'],
 })
 export class WeatherComponent implements OnInit {
-
-  constructor(private weatherService: WeatherService) {
-
-  }
+  constructor(private weatherService: WeatherService) {}
   ngOnInit(): void {
     this.weatherService.findWeather('30034');
   }
-  }
+}
