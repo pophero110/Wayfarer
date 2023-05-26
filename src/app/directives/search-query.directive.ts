5d89b62a3b251950a54f8be0e30c8e9b6d12c534
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-25 22:09:59 -0500

    Refactor SearchQueryDirective
    1. add findPostByNameOrContentAndShowPostPage method

--- src/app/directives/search-query.directive.ts
+++ src/app/directives/search-query.directive.ts
@@ -1,12 +1,17 @@
 import { Directive, HostListener } from '@angular/core';
 import { CityService } from '../services/city.service';
+import { PostService } from '../services/post.service';
 import { Router } from '@angular/router';
 
 @Directive({
   selector: '[appSearchQuery]',
 })
 export class SearchQueryDirective {
-  constructor(private cityService: CityService, private router: Router) {}
+  constructor(
+    private cityService: CityService,
+    private postService: PostService,
+    private router: Router
+  ) {}
 
   @HostListener('submit', ['$event'])
   onSubmit(event: Event) {
@@ -15,14 +20,31 @@ export class SearchQueryDirective {
         'input'
       );
       if (inputElement !== null) {
-        let cityName = inputElement.value;
-        let city = this.cityService.findCityByName(cityName);
-        if (city) {
-          this.router.navigate(['/cities/' + city.id]);
-        } else {
-          alert('The city can not found with name ' + cityName);
-        }
+        let userSearchText = inputElement.value;
+        if (this.findCityByNameAndShowCityPage(userSearchText)) return;
+        if (this.findPostByNameOrContentAndShowPostPage(userSearchText)) return;
+        alert(
+          `Can not find any post or city with given text '${userSearchText}'`
+        );
       }
     }
+  }
+
+  private findCityByNameAndShowCityPage(text: string): boolean {
+    let city = this.cityService.findCityByName(text);
+    if (city) {
+      this.router.navigate(['/cities/' + city.id]);
+      return true;
+    }
+    return false;
+  }
+
+  private findPostByNameOrContentAndShowPostPage(text: string): boolean {
+    let post = this.postService.findPostByTitleOrContentOrAuthor(text);
+    if (post) {
+      this.router.navigate(['/posts/' + post.id]);
+      return true;
+    }
+    return false;
   }
 }
