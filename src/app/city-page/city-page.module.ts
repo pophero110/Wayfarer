d84532b04da956e222893c5cbd74c1c34e68d66d
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-26 20:01:47 -0500

    Change weather component import path

--- src/app/city-page/city-page.module.ts
+++ src/app/city-page/city-page.module.ts
@@ -4,10 +4,15 @@ import { CityDetailComponent } from './city-detail/city-detail.component';
 import { CityListComponent } from './city-list/city-list.component';
 import { CityPageComponent } from './city-page-component/city-page.component';
 import { RoutingModule } from '../routing/routing.module';
-import { WeatherComponent } from '../weather/weather.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
-  declarations: [CityDetailComponent, CityListComponent, CityPageComponent, WeatherComponent],
+  declarations: [
+    CityDetailComponent,
+    CityListComponent,
+    CityPageComponent,
+    WeatherComponent,
+  ],
   imports: [CommonModule, RoutingModule],
   exports: [CityDetailComponent, CityListComponent, CityPageComponent],
 })
