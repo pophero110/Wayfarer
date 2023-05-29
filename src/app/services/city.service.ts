cde1d19fffbecec96780cc0dc9b88d9f2921d02b
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-29 17:48:35 -0500

    Add docstring

--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -8,15 +8,32 @@ import { Post, posts } from '../data/posts';
 export class CityService {
   constructor() {}
 
+  /**
+   * Finds a city by its ID.
+   *
+   * @param {string} cityId - The ID of the city to find.
+   * @returns {City | undefined} - The city object if found, undefined otherwise.
+   */
   findCityById(cityId: string): City | undefined {
     return cities.find((city) => city.id === cityId);
   }
 
+  /**
+   * Finds posts associated with a specific city ID.
+   *
+   * @param {string} cityId - The ID of the city to find posts for.
+   * @returns {Post[]} - An array of posts associated with the city ID.
+   */
   findPostsByCityId(cityId: string): Post[] {
     return posts.filter((post) => post.cityId === cityId);
   }
 
-  // allow user to find city by name
+  /**
+   * Finds a city by its name, case-insensitive.
+   *
+   * @param {string} name - The name of the city to find.
+   * @returns {City | undefined} - The city object if found, undefined otherwise.
+   */
   findCityByName(name: string): City | undefined {
     return cities.find(
       (city) => city.name.toLowerCase() === name.toLowerCase()
