1fc19e297dd64f084dfa836c5ee88f9ead061841
vnpugh <vnp19@yahoo.com>
2023-06-12 19:09:46 -0500

    Initial File Setup

--- src/app/directives/search-query.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { SearchQueryDirective } from './search-query.directive';
-
-// describe('SearchQueryDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new SearchQueryDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
